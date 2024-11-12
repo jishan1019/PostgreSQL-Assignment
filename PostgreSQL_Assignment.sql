525d81f94d57b8006269ce3ea154d40c442c3003
jishan1019 <imransilamjishan80@gmail.com>
2024-11-13 00:42:10 +0600

    query 1 solve

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,12 @@
+-- Query 1:
+INSERT INTO students(student_name, age, email, frontend_mark, backend_mark, status)
+    VALUES ('Mehedi', 23, 'mehedi@gmail.com', 50, 47, NULL)
+
+
+--Explain Query 1 purpose and functionality:
+
+
+
+-- Query 2
+
+SELECT * FROM students
