8f542911be2a082a6d6a03c1079cfa31495240c8
jishan1019 <imransilamjishan80@gmail.com>
2024-11-13 00:32:09 +0600

    course table example data create

--- courses_table.sql
+++ courses_table.sql
@@ -3,3 +3,12 @@ CREATE TABLE courses (
     course_name VARCHAR(100),
     credits INTEGER
 );
+
+
+INSERT INTO courses (course_name, credits) VALUES
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
+
+SELECT * FROM courses
